8570aebc94a6429711498ba6d03939cf0cd46dd3
START PG <kngladsub@outlook.com>
2021-08-03 21:57:02 +0900

    commit20210803-01

---
+++ CMS_3D_Core/CMS_3D_Core/Data/Migrations/20210803111902_migr_defaultcn_2021080301.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CMS_3D_Core.Data.Migrations
+{
+    public partial class migr_defaultcn_2021080301 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ CMS_3D_Core/CMS_3D_Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CMS_3D_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
