3831ea66195d742ea63bacd2dced75be5f4d7a51
START PG <kngladsub@outlook.com>
2021-08-17 21:33:29 +0900

    commit20210817-01
    スキャフォールディングで「Identity.Login,Identity.Register」を追加。
    この時点では自動追加分のみ。
    差分を見るためにいったんコミット

---
+++ CMS_3D_Core/CMS_3D_Core/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CMS_3D_Core.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CMS_3D_Core.Areas.Identity.IdentityHostingStartup))]
+namespace CMS_3D_Core.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
