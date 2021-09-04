b1ea29d38c23dc4cbddb7e6a3493c46a6eeaacc2
START PG <kngladsub@outlook.com>
2021-09-04 18:22:14 +0900

    commit20210904-03
    権限設定漏れを修正

--- CMS_3D_Core/CMS_3D_Core/Controllers/SiteSettingController.cs
+++ CMS_3D_Core/CMS_3D_Core/Controllers/SiteSettingController.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 using CMS_3D_Core.Models.EDM;
 
 namespace CMS_3D_Core.Controllers
 {
+    [Authorize]
     public class SiteSettingController : Controller
     {
         private readonly db_data_coreContext _context;
